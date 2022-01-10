361163dc663a24f4589dfd940a100cd4866df9a6
maskedball <maskedballmail@gmail.com>
2022-01-10 09:42:26 +0300

    delete unused network services

--- Common/Services/INetworkService.cs
+++
@@ -1,8 +0,0 @@
-namespace Common.Services;
-
-public interface INetworkService
-{
-    Task<string> GetResponse(string url);
-    Task SendPostRequest(string url);
-    Task SendPostRequestWithJsonBody<T>(string url, T model);
-}
