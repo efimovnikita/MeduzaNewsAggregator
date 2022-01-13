9633a64878d4c8d1f8d725bd50d286e9afd3ee51
maskedball <maskedballmail@gmail.com>
2022-01-13 14:43:21 +0300

    add new microservice and pulse page

--- NewsAggregatorWebWASMUI/Program.cs
+++ NewsAggregatorWebWASMUI/Program.cs
@@ -1,6 +1,6 @@
-using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
 using NewsAggregatorWebWASMUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,7 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped(_ => new HubConnectionBuilder().WithUrl("https://localhost:7172/headings").Build());
 
 await builder.Build().RunAsync();
