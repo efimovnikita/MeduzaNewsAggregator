0df346379f7f08680e0748df3d905c536f248b4e
maskedball <maskedballmail@gmail.com>
2022-01-19 12:02:10 +0300

    prepare microservice for deploy

--- NewsAggregatorWebWASMUIv2/Program.cs
+++ NewsAggregatorWebWASMUIv2/Program.cs
@@ -14,14 +14,14 @@ builder.Services.AddScoped<IState, AppState>();
 builder.Services.AddSingleton(_ => 
 { 
     var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-    var uri = new Uri("https://localhost:7055");
+    var uri = new Uri("http://20.113.15.205:80");
     var channel = GrpcChannel.ForAddress(uri, new GrpcChannelOptions { HttpClient = httpClient }); 
     return new HeadingsService.HeadingsServiceClient(channel); 
 });
 builder.Services.AddSingleton(_ => 
 { 
     var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-    var uri = new Uri("https://localhost:7055");
+    var uri = new Uri("http://20.113.15.205:80");
     var channel = GrpcChannel.ForAddress(uri, new GrpcChannelOptions { HttpClient = httpClient });
     return new ArticlesService.ArticlesServiceClient(channel);
 });
