5a568f89476e9e52ecfd8d0570306c26867000d5
maskedball <maskedballmail@gmail.com>
2022-01-20 14:59:43 +0300

    add System.Commandline package

--- NewsAggregatorConsoleTool/App.cs
+++ NewsAggregatorConsoleTool/App.cs
@@ -1,3 +1,4 @@
+using System.CommandLine;
 using Common.HelperMethods;
 using Common.Models;
 
@@ -11,31 +12,36 @@ public class App
     {
         _httpClientFactory = httpClientFactory;
     }
+
     public async Task Run(string[] args)
     {
-        var category = string.Empty;
-        var newsCount = string.Empty;
+        var argument = new Argument<string>("action", () => "show",
+            "Choice of action. You can either \"show\" a list of news headlines or \"search\" for news.");
+        var categoryOption = new Option<string>(new[] { "--category" }, () => "news", "News category.");
+        var queryOption = new Option<string>(new[] { "--query", "-q" }, "Search query.");
+        var countOption = new Option<int>("--count", () => 10);
 
-        switch (args.Length)
+        var rootCommand = new RootCommand {argument, categoryOption, countOption, queryOption};
+        rootCommand.SetHandler<string, string, string, int>(Handler, argument, categoryOption, queryOption,
+            countOption);
+
+        rootCommand.Description = "Utility to search for news on the Meduza website.";
+
+        await rootCommand.InvokeAsync(args);
+    }
+
+    private async Task Handler(string action, string category, string query, int newsCount)
+    {
+        if (action.Equals("search", StringComparison.InvariantCultureIgnoreCase))
         {
-            case 0:
-                Console.Write("Category: ");
-                category = Console.ReadLine()!.Trim();
-                newsCount = "10";
-                break;
-            case 1:
-                category = args[0];
-                newsCount = "10";
-                break;
-            case 2:
-                category = args[0];
-                newsCount = args[1];
-                break;
+            Console.WriteLine($"Search query is {query}");
+            Console.WriteLine("Search doesn't work yet...");
+            return;
         }
 
-        var headingsData = await Methods.GetHeadingsDataModel(_httpClientFactory, "headings", category) ?? new HeadingsDataModel();
-        var headingModels = headingsData.Documents
-            .Select(pair => pair.Value)
+        var headingsData = await Methods.GetHeadingsDataModel(_httpClientFactory, "headings", category) ??
+                           new HeadingsDataModel();
+        var headingModels = headingsData.Documents.Select(pair => pair.Value)
             .Take(Convert.ToInt32(newsCount))
             .ToList();
 
@@ -43,7 +49,5 @@ public class App
         {
             Console.WriteLine($"{i + 1}) {headingModels[i].Title}");
         }
-
-        Console.ReadLine();
     }
 }
